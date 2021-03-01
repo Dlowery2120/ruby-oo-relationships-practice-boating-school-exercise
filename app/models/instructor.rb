01dae4a8ad6cb02b227e31c7ff33a15e82083250
Devante Lowery <dlowery2120@gmail.com>
2021-03-01 00:13:36 -0600

    commit tree fa47af020f19cf0d58ef651eb3856d514dfc6b5d

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,23 @@
 class Instructor
+    attr_accessor :name
 
+    def initialize (name)
+        @name = name
+    end
+
+    def passed_students
+
+    end
+
+    def pass_student
+
+    end
+
+    def fail_student
+
+    end
+    
+    def self.all
+    @@all
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Student
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def add_boating_test(name)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def all_instructors
+        
+    end
+
+    def find.Student
+
+    end
+
+    def grade_percentage
+
+    end
+
+    def self.all
+        @@all
+    end
+
 
 end
+binding.pry
